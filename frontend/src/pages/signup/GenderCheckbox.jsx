df403e00f17801725bb0ab78e0b113fcceb8233f
Anmol Singh <as0796348@gmail.com>
2024-03-07 20:41:05 +0530

    Gender added

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,16 +1,16 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({onCheckboxChange, selectedGender}) => {
     return (
         <div className='flex'>
             <div className='form-control'>
-                <label className='label gap-2 cursor-pointer'>
-                    <input type="checkbox" className='checkbox border-white' />
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""}`}>
+                    <input type="checkbox" className='checkbox border-white' checked={selectedGender === "male"} onChange={() => onCheckboxChange("male")} />
                     <span className='label-text text-white bg-transparent'>Male</span>
                 </label>
             </div>
 
             <div className='form-control'>
-                <label className='label gap-2 cursor-pointer'>
-                    <input type="checkbox" className='checkbox border-white' />
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "female" ? "selected" : ""}`}>
+                    <input type="checkbox" className='checkbox border-white' checked={selectedGender === "female"} onChange={() => onCheckboxChange("female")} />
                     <span className='label-text text-white'>Female</span>
                 </label>
             </div>
