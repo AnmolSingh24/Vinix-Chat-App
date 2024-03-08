5d8dca1924ab3de07b82696b5a637641c6657d59
Anmol Singh <as0796348@gmail.com>
2024-03-08 20:42:39 +0530

    AI search added

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,26 +1,23 @@
-import Conversation from './Conversation'
+import Conversation from './Conversation';
 import useGetConversations from '../../hooks/useGetConversations';
 
 const Conversations = () => {
   const { loading, conversations } = useGetConversations();
-
-  console.log("Conversations: ", conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-      {loading ? <span className='loading loading-spinner mx-auto'></span> : conversations.map((conversation, idx) => {
+      {loading ? <span className='loading loading-spinner mx-auto'></span> : conversations.map((conversation, idx) => (
         <Conversation
           key={conversation._id}
           conversation={conversation}
           lastIdx={idx === conversations.length - 1}
         />
-      })}
-
-      {/* {loading ? <span className='loading loading-spinner mx-auto'></span> : null} */}
+      ))}
     </div>
   )
 }
 
 export default Conversations;
+
 
 
 
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,16 @@
 import { IoSearchSharp } from "react-icons/io5";
+import { SiChatbot } from "react-icons/si";
 
 const SearchInput = () => {
   return (
     <form className='flex items-center gap-2'>
-        <input type="text" placeholder='Search.....' className='input input-bordered rounded-full bg-white' />
-        <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
+      <input type="text" placeholder='Search.....' className='input input-bordered rounded-full bg-white' />
+      <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
         <IoSearchSharp className='w-6 h-6 outline-none' />
-        </button>
+      </button>
+      <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
+        <SiChatbot className='w-6 h-6 outline-none' />
+      </button>
     </form>
   )
 }
