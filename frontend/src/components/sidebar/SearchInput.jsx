414fc724048512e61156bccb915276d971ce43a3
Anmol Singh <as0796348@gmail.com>
2024-03-09 20:37:23 +0530

    background added

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,10 +5,10 @@ const SearchInput = () => {
   return (
     <form className='flex items-center gap-2'>
       <input type="text" placeholder='Search.....' className='input input-bordered rounded-full bg-white' />
-      <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
+      <button type='submit' className='btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none'>
         <IoSearchSharp className='w-6 h-6 outline-none' />
       </button>
-      <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
+      <button type='submit' className='btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none'>
         <SiChatbot className='w-6 h-6 outline-none' />
       </button>
     </form>
