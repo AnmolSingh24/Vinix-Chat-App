86d289ccc574f953146c245ba761f5545dd30c0b
Anmol Singh <as0796348@gmail.com>
2024-03-12 22:51:12 +0530

    changes added

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '120d'
-    })
+    });
 
     res.cookie("jwt", token,{
         maxAge: 120 * 24 * 60 * 1000,  //MS
-        httpOnly: true, //prevent XSS attacks cross-site scripting attacks
+        httpOnly: false, //prevent XSS attacks cross-site scripting attacks
         sameSite: "strict", //CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !== "development",
-    } )
+    });
 }
 
 export default generateTokenAndSetCookie;
