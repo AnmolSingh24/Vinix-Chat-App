f59370b6a15647500b33355396a6bdca4b5afe98
Anmol Singh <as0796348@gmail.com>
2024-03-18 02:59:33 +0530

    AI Chatbot added

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-import { SiChatbot } from "react-icons/si";
+import { AISearch } from "../sidebar/AISearch";
 import useConversation from "../../zustand/useConversation";
 import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
@@ -33,31 +33,10 @@ const SearchInput = () => {
       <button type='submit' className='btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none'>
         <IoSearchSharp className='w-6 h-6 outline-none' />
       </button>
-      <button type='submit' className='btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none'>
-        <SiChatbot className='w-6 h-6 outline-none' />
-      </button>
+
+      <AISearch/>
     </form>
   )
 }
 
-export default SearchInput
-
-
-
-
-//STARTER CODE FOR THE SEARCH INPUT
-
-// import { IoSearchSharp } from "react-icons/io5";
-
-// const SearchInput = () => {
-//   return (
-//     <form className='flex items-center gap-2'>
-//         <input type="text" placeholder='Search.....' className='input input-bordered rounded-full bg-white' />
-//         <button type='submit' className='btn btn-circle bg-green-400 text-white hover:bg-emerald-500 border-none'>
-//         <IoSearchSharp className='w-6 h-6 outline-none' />
-//         </button>
-//     </form>
-//   )
-// }
-
-// export default SearchInput
+export default SearchInput;
