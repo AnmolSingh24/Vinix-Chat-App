e4803bebee0269f6d0281aeaa6f314c6cb040bc8
Anmol Singh <as0796348@gmail.com>
2024-03-28 12:05:05 +0530

    snippet removed

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -19,29 +19,3 @@ const GenderCheckbox = ({onCheckboxChange, selectedGender}) => {
 }
 
 export default GenderCheckbox
-
-
-
-// STARTER FOR THE GENDER CHECKBOX COMPONENT
-
-// const GenderCheckbox = () => {
-//     return (
-//         <div className='flex'>
-//             <div className='form-control'>
-//                 <label className='label gap-2 cursor-pointer'>
-//                     <input type="checkbox" className='checkbox border-white' />
-//                     <span className='label-text text-white bg-transparent'>Male</span>
-//                 </label>
-//             </div>
-
-//             <div className='form-control'>
-//                 <label className='label gap-2 cursor-pointer'>
-//                     <input type="checkbox" className='checkbox border-white' />
-//                     <span className='label-text text-white'>Female</span>
-//                 </label>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default GenderCheckbox
