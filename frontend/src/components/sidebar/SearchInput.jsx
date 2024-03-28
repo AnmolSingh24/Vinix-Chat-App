2265125f11cc73b060e768ec11bf097b5d11e65b
Anmol Singh <as0796348@gmail.com>
2024-03-28 10:20:53 +0530

    DropDown list added

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-import AISearch from "../sidebar/AISearch";
 import useConversation from "../../zustand/useConversation";
 import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
+import DropDownOptions from "../sidebar/dropDown/DropDownOptions";
 
 const SearchInput = () => {
-  const [ search, setSearch ] = useState("");
+  const [search, setSearch] = useState("");
   const { setSelectedConversation } = useConversation();
   const { conversations } = useGetConversations();
 
@@ -15,29 +15,37 @@ const SearchInput = () => {
 
     if (!search) return;
     if (search.length < 3) {
-      return toast.error("Search must me atleast 3 characters long")
+      return toast.error("Search must be at least 3 characters long");
     }
 
-    const conversation = conversations.find((c) => c.fullname.toLowerCase().includes(search.toLowerCase()));
+    const conversation = conversations.find((c) =>
+      c.fullname.toLowerCase().includes(search.toLowerCase())
+    );
     if (conversation) {
       setSelectedConversation(conversation);
-      setSearch('');
+      setSearch("");
     } else toast.error("No users found");
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit} className='flex items-center gap-2'>
-      <input type="text" placeholder='Search.....' className='input input-bordered rounded-full bg-white'
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search....."
+        className="input input-bordered rounded-full bg-white"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button type='submit' className='btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none'>
-        <IoSearchSharp className='w-6 h-6 outline-none' />
+      <button
+        type="submit"
+        className="btn btn-circle bg-emerald-500 text-white hover:bg-emerald-500 border-none"
+      >
+        <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
 
-      <AISearch/>
+      <DropDownOptions />
     </form>
-  )
-}
+  );
+};
 
 export default SearchInput;
