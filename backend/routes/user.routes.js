e4f3a38728c361ecb32c5387180f2d8e42e056f4
Anmol Singh <as0796348@gmail.com>
2024-04-02 18:27:05 +0530

    routes added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import { getUsersForSidebar } from "../controllers/user.controller.js";
+import { createGroup } from "../controllers/group.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute , getUsersForSidebar);
+router.post("/groups", protectRoute , createGroup);
 
 export default router;
