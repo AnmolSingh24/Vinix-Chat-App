01041f86fbe112d4172558e12c799d7274246e46
Anmol Singh <as0796348@gmail.com>
2024-04-02 18:32:50 +0530

    changes added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import MessageContainer from '../../components/messages/MessageContainer'
+import useListenGroup from '../../hooks/useListenGroup'
 
 const Home = () => {
+
+  const newGroup = useListenGroup();
+  console.log(newGroup);
+
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
