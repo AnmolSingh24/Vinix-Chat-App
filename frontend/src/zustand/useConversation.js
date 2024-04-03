7ef2ac547e0b54d4c1b9a09a581c9295bad47c01
Anmol Singh <as0796348@gmail.com>
2024-04-03 11:00:43 +0530

    useGroup added

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,18 +1,16 @@
 import { create } from "zustand";
 
-const useConversation = create ((set) => ({
+export const useConversation = create((set) => ({
     selectedConversation: null,
-    setSelectedConversation: (selectedConversation) => set({selectedConversation}),
+    setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
     messages: [],
-    setMessages: (messages) => set({messages}),
+    setMessages: (messages) => set({ messages }),
 }));
 
-export default useConversation;
 
-
-// export const useGroup = create ((set) => ({
-//     selectedGroup: null,
-//     setSelectedGroup: (selectedGroup) => set({selectedGroup}),
-//     groups: [],
-//     setGroups: (groups) => set({groups}),
-// }));
+export const useGroup = create((set) => ({
+    selectedGroup: null,
+    setSelectedGroup: (selectedGroup) => set({ selectedGroup }),
+    groups: [],
+    setGroups: (groups) => set({ groups }),
+}));
