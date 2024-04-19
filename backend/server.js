70d2dae5e083cbba8bce1962e74afd62e3487b2b
Anmol Singh <as0796348@gmail.com>
2024-04-19 19:20:14 +0530

    changes added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
@@ -9,6 +10,7 @@ import { app, server } from "./socket/socket.js";
 import bodyParser from "body-parser"
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(bodyParser.json({limit: '50mb', extended: true}));
@@ -20,6 +22,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
